c454683d3f198e4f3368c2b1251b62a0810f2b2c
Bedirhan Şık <Bedirhan_98@outlook.com>
2022-03-11 16:32:13 +0300

    Add files via upload

---
+++ My_First/Bedirhan.cpp
@@ -0,0 +1,40 @@
+// BEDÝRHAN ÞIK
+// 160106109007
+// 2017-2018 BAHAR DÖNEMÝ
+// ALGORÝTMA VE PROGRAMLAMA II DERSÝ
+// I. ÖÐRETÝM
+// VÝZE-PROJE
+
+
+#include <iostream>
+#include <string.h>
+#include <time.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include<locale.h>
+
+#include "giris.h"
+#include "menu.h" 
+
+
+using namespace std;
+// OTOMASYON
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(int argc, char** argv) {
+	int x;
+	setlocale(LC_ALL, "Turkish");
+	giriss();
+	menu1();
+	
+	
+	
+	
+	//system("cls");
+	getch();
+	return 0;
+}
+
+
---
+++ My_First/giris.h
@@ -0,0 +1,62 @@
+#ifndef giris
+#define giris
+
+void giriss()
+	{
+		printf("\n");
+	printf("               ");
+	printf("                         ");
+	printf("                        *  *  *  *  *   *                 \n");
+	printf("               ");
+	printf("                         ");
+	printf("                     *                     *             \n");
+	printf("               ");
+	printf("                         ");
+	printf("                  *                           *            \n");	
+	printf("    --- BEÞÝKTAÞ KULÜBÜNE HOÞ GELDÝNÝZ ---");
+	printf("                 *      B     J     K      *            \n");
+	printf("               ");
+	printf("                         ");
+	printf("                    *                       *             \n");
+	printf("               ");
+	printf("                         ");
+	printf("                     *                     *             \n");
+	printf("               ");
+	printf("                         ");
+	printf("                     *          *          *            \n");
+	printf("               ");
+	printf("                         ");
+	printf("                    *       .       .       *            \n");
+	printf("               ");
+	printf("                         ");
+	printf("                   *       .         .       *            \n");
+	printf("               ");
+	printf("                         ");
+	printf("                  *         .       .         *           \n");
+	printf("               ");
+	printf("                         ");
+	printf("                  *           .   .           *           \n");
+	printf("               ");
+	printf("                         ");
+	printf("                  *                           *            \n");
+	printf("               ");
+	printf("                         ");
+	printf("                   *      1   9   0   3      *           \n");
+	printf("               ");
+	printf("                         ");
+	printf("                     *                     *              \n");
+	printf("               ");
+	printf("                         ");
+	printf("                        *               *                \n");
+	printf("               ");
+	printf("                         ");
+	printf("                            *       *                \n");
+	printf("               ");
+	printf("                         ");
+	printf("                                *                        \n\n\n\n\n\n\n\n\n");
+	printf("  Devam etmek için herhangi bir tuþa basýnýz.. ");
+	getch();
+	//
+	
+	}
+#endif
